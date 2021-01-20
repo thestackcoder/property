860a482aec3d67a1379845cd2f03dc3e67e494c0
Fahad Ahmed <fahadhmed2496@gmail.com>
2021-01-21 03:04:51 +0500

    models

--- project/models.py
+++ project/models.py
@@ -39,9 +39,26 @@ class SmallText(models.Model):
     def __str__(self):
         return self.title
 
+
+class FloorPlan(models.Model):
+    image = models.ImageField(upload_to='media')
+
+
+class FloorPlanSection(models.Model):
+    image = models.ImageField(upload_to='media')
+    title = models.CharField(max_length=255)
+    boundaries = models.CharField(max_length=50)
+    floorplan = models.OneToOneField("project.FloorPlan", on_delete=models.CASCADE)
+
 class CarouselItem(models.Model):
     desc = models.TextField()
     project = models.ForeignKey("project.Project", related_name='carousel_items', on_delete=models.CASCADE)
 
 
-    
+class CarouselPhoto(models.Model):
+    photo = models.ImageField(upload_to='media')
+    carouselItem = models.ForeignKey("project.CarouselItem", on_delete=models.CASCADE)
+
+class CarouselVideo(models.Model):
+    video = models.FileField(upload_to='media/files')
+    carouselItem = models.ForeignKey("project.CarouselItem", on_delete=models.CASCADE)
